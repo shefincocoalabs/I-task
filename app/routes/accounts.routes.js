5e3023511b4c2de07f054865be2239e1d32ad228
shefin <shefin@cocoalabs.in>
2020-04-01 18:54:52 +0530

    api changes

--- app/routes/accounts.routes.js
+++ app/routes/accounts.routes.js
@@ -2,6 +2,6 @@ module.exports = (app,methods,options) => {
     const accounts = methods.loadController('accounts',options);
     accounts.methods.post('/sign-up',accounts.signUp, {auth:false});
     accounts.methods.post('/login',accounts.login, {auth:false});
-    accounts.methods.post('/update-profile',accounts.updateProfile, {auth:true});
+    accounts.methods.patch('/update-profile',accounts.updateProfile, {auth:true});
     
 }
