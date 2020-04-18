739db7fa060b0c25b76ac579ab33fcadcbfd5987
shefin <shefin@cocoalabs.in>
2020-04-18 23:07:00 +0530

    changes

--- config/app.config.js
+++ config/app.config.js
@@ -1,7 +1,6 @@
-const path = require('path');
-var fs = require('fs');
-console.log(__dirname + '../../');
-console.log('dirname');
+
+// console.log(__dirname + '../../');
+// console.log('dirname');
 var relativeCommonPath = "../I-task-uploads/";
 module.exports = {
   gateway: {
