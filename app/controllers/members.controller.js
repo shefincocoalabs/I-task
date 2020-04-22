153cc278455ba38e6f387723b9c8a7083e874b52
shefin <shefin@cocoalabs.in>
2020-04-22 17:33:54 +0530

    changes

--- app/controllers/members.controller.js
+++ app/controllers/members.controller.js
@@ -59,6 +59,7 @@ function memberController(methods, options) {
       fullName: fullName,
       email: email,
       phone: phone,
+      image: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
       position: position,
       password: password,
       createdBy: userId,
