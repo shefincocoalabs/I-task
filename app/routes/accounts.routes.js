ff09f5ce4dc5fbbc0ae155b2c4ddd9fcf61ccb7b
shefin <shefin@cocoalabs.in>
2020-04-24 20:11:54 +0530

    changes

--- app/routes/accounts.routes.js
+++ app/routes/accounts.routes.js
@@ -1,18 +1,12 @@
 const auth = require('../middleware/auth.js');
 var multer = require('multer');
-var crypto = require('crypto');
-var mime = require('mime-types');
 var config = require('../../config/app.config.js');
 var profileConfig = config.users;
 
 var storage = multer.diskStorage({
     destination: profileConfig.imageUploadPath,
     filename: function (req, file, cb) {
-        crypto.pseudoRandomBytes(16, function (err, raw) {
-            if (err) return cb(err)
-
-            cb(null, raw.toString('hex') + "." + mime.extension(file.mimetype))
-        })
+        cb(null, file.fieldname + '-' + Date.now())
     }
 });
 var userImageUpload = multer({ storage: storage });
