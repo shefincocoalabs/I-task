7a16cdf2d1e1f0db0a12aa41050de32956f5eee0
shefin <shefin@cocoalabs.in>
2020-04-24 11:11:03 +0530

    changes

--- config/app.config.js
+++ config/app.config.js
@@ -1,3 +1,4 @@
+var commonStorePath = 'http://172.104.61.150/I-task/'
 module.exports = {
   gateway: {
     url: "http://localhost:5000"
@@ -9,14 +10,17 @@ module.exports = {
     imageUploadPath: '/var/www/html/I-task/images/profile-images/'
   },
   members: {
+    imageBase: commonStorePath + 'images/member-images/',
     imageUploadPath: '/var/www/html/I-task/images/member-images/',
     resultsPerPage: 30,
   },
   tasks: {
+    fileBase: commonStorePath + 'files/task-documents/',
     documentsUploadPath: '/var/www/html/I-task/files/task-documents/',
     resultsPerPage: 30,
   },
   projects: {
+    fileBase: commonStorePath + 'files/project-documents/',
     documentsUploadPath: '/var/www/html/I-task/files/project-documents/',
     resultsPerPage: 30,
   },
