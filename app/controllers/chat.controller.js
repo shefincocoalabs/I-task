777d5776a25f5ded61444eed6208aabb678f3396
shefin <shefin@cocoalabs.in>
2020-05-15 14:15:36 +0530

    changes

--- app/controllers/chat.controller.js
+++ app/controllers/chat.controller.js
@@ -6,15 +6,18 @@ res.send('hiii');
 exports.chat = function (io) {
   io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('joined', function (data) {
-      console.log(data);
-      socket.emit('acknowledge', 'Acknowledged');
-    });
-    socket.on('chat message', function (msg) {
-      console.log('message: ' + msg);
-      socket.emit('response message', msg + '  from server');
-      //socket.broadcast.emit('response message', msg + '  from server');
-    });
+    // socket.on('joined', function (data) {
+    //   console.log(data);
+    //   socket.emit('acknowledge', 'Acknowledged');
+    // });
+    // socket.on('chat message', function (msg) {
+    //   console.log('message: ' + msg);
+    //   socket.emit('response message', msg + '  from server');
+    //   //socket.broadcast.emit('response message', msg + '  from server');
+    // });
+    socket.on("send_message", (data) => {
+      socket.broadcast.emit("receive_message", data)
+  })
   });
 };
 
